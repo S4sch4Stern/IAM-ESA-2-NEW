9abc8b1788fc306ed1d4efd36310dead8b91f724
Alexander Thofern <AlexanderThofern@gmx.de>
2024-05-19 15:29:50 +0200

    first commit, added new skeleton

---
+++ MWF-NEW/org.dieschnittstelle.iam.mwf.skeleton-master/www/js/model/MyEntities.js
@@ -0,0 +1,27 @@
+/**
+ * @author Jörn Kreutel
+ *
+ * this skript defines the data types used by the application and the model operations for handling instances of the latter
+ */
+
+
+import {mwfUtils} from "../Main.js";
+import {EntityManager} from "../Main.js";
+
+/*************
+ * example entity
+ *************/
+
+export class MyEntity extends EntityManager.Entity {
+
+    // TODO-REPEATED: declare entity instance attributes
+
+    constructor() {
+        super();
+    }
+
+}
+
+// TODO-REPEATED: add new entity class declarations here
+
+
